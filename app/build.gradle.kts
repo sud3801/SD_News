9f79be1ccfc7a8fd45740973078b84594e6bc65c
SD <sudarshan.cse065@gmail.com>
2026-04-06 21:42:10 +0530

    "bump version to 1.6.2" Commit
    - Increment `versionCode` to 5 and `versionName` to "1.6.2" in `app/build.gradle.kts`.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.sd.sdnews"
         minSdk = 24
         targetSdk = 35
-        versionCode = 4
-        versionName = "1.6.1"
+        versionCode = 5
+        versionName = "1.6.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
